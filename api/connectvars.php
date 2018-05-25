ac744750beba40f521e0d1e8a0769672cf1d5dcf
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2018-05-24 23:36:44 -0700

    css and component for expense

---
+++ api/connectvars.php
@@ -0,0 +1,9 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
+  define('DB_USER', 'cs340_username');
+  define('DB_PASSWORD', 'YOUR PASSWORD');
+  define('DB_NAME', 'cs340_username');
+  define('CON_STRING', 'mysql:host=classmysql.engr.oregonstate.edu;dname=cs340_username');
+?>
+
--- client/src/group/expense.tsx
+++ client/src/group/expense.tsx
@@ -5,10 +5,38 @@ import "./expense.css";
 export interface ExpenseProps {
   expense: ExpenseModel;
   onPay: (expense: ExpenseModel) => void;
+  hasPaid: boolean;
 }
 
 export const Expense: React.SFC<ExpenseProps> = props => {
+  let payNowBtn: JSX.Element;
+  if (props.hasPaid) {
+    payNowBtn = <div className="expense-paid">All paid up!</div>;
+  } else {
+    payNowBtn = (
+      <button
+        className="expense-pay-button"
+        onClick={() => props.onPay(props.expense)}
+      >
+        Pay ${props.expense.eCostTotal/}
+      </button>
+    );
+  }
+
+  const havePaidUsers = props.expense.paid.map(p => p.username).join(", ");
+
   return (
-    <div className=""
-  )
-}
+    <div className="expense">
+      <div className="expense-header">
+        <div>
+          <h4>{props.expense.eName}</h4>
+          <p>{props.expense.eDesc}</p>
+        </div>
+        <div />
+      </div>
+      <div className="expense-body">
+        <p>{havePaidUsers} have paid.</p>
+      </div>
+    </div>
+  );
+};
