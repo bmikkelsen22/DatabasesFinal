f5c602a403b8b5f73f4e49e8d8c821d1d70c9f68
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2018-05-31 16:25:47 -0700

    removed comments

--- api/addexpense.php
+++ api/addexpense.php
@@ -16,45 +16,42 @@ $nextEid = $eidRes['nextEid'];
 # Get JSON as a string
 $jsonStr = file_get_contents('php://input');
 
-//testing...
-echo $jsonStr;
-//error_reporting(E_ALL);
 //load and check json payload
 $json = json_decode($jsonStr, TRUE);
 echo $json['eName'];
-// $eName = $json['eName'];
-// $eDesc = $json['eDesc'];
-// $eCost = $json['eCostTotal'];
-// $eNumUsers = $json['eNumUsers'];
-// $gID = $json['gID'];
+$eName = $json['eName'];
+$eDesc = $json['eDesc'];
+$eCost = $json['eCostTotal'];
+$eNumUsers = $json['eNumUsers'];
+$gID = $json['gID'];
 
-// //insert expense
-// $query = "INSERT INTO Expenses (eName, eDesc, eID, eCostTotal, eNumUsers, gID) VALUES ($eName, $eDesc, $nextEid, $eCost, $eNumUsers, $gID)";
+//insert expense
+$query = "INSERT INTO Expenses (eName, eDesc, eID, eCostTotal, eNumUsers, gID) VALUES ($eName, $eDesc, $nextEid, $eCost, $eNumUsers, $gID)";
 
-// if ($conn->query($query) !== TRUE) {
-//   http_response_code(500);
-//   die("Error adding expense: $query \n".$query.);
-// }
+if ($conn->query($query) !== TRUE) {
+  http_response_code(500);
+  die("Error adding expense: $query \n".$query.);
+}
 
-// //insert expensePaid
+//insert expensePaid
 
-// $query = "INSERT INTO ExpensesPaid (eID, username, pPaid) VALUES ";
-// for ($i = 0; $i < count($json['users']); $i++) {
-//   $user = $json['users'][$i];
+$query = "INSERT INTO ExpensesPaid (eID, username, pPaid) VALUES ";
+for ($i = 0; $i < count($json['users']); $i++) {
+  $user = $json['users'][$i];
 
-//   $username = $user['username'];
-//   $pPaid = $user['pPaid'];
+  $username = $user['username'];
+  $pPaid = $user['pPaid'];
 
-//   $query .= "($nextEid, $username, $pPaid)";
-//   if ($i < count($json['users']) - 1) {
-//     $query .= ", ";
-//   }
-// }
+  $query .= "($nextEid, $username, $pPaid)";
+  if ($i < count($json['users']) - 1) {
+    $query .= ", ";
+  }
+}
 
-// if ($conn->query($query) !== TRUE) {
-//   http_response_code(500);
-//   die("Error adding expensesPaid:".$query);
-// }
+if ($conn->query($query) !== TRUE) {
+  http_response_code(500);
+  die("Error adding expensesPaid:".$query);
+}
 
-// echo("Added expense");
+echo("Added expense");
 ?>
