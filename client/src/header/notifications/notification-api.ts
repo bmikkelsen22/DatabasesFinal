40041aad24106b06b1db9a5187e7c9ab987c26bf
Parker Okonek <okonekp@oregonstate.edu>
2018-06-02 20:13:34 -0700

    Possibly fixed the notification api

--- client/src/header/notifications/notification-api.ts
+++ client/src/header/notifications/notification-api.ts
@@ -1,7 +1,7 @@
-import { MemberModel, NotificationsModel } from "../../models";
+import { MemberModel, NotificationModel } from "../../models";
 import { getRequest, postRequest } from "../../api-calls";
 
-export async function getNotificationList(nReceiver?: string): Promise<NotificationsModel[]> {
+export async function getNotificationList(nReceiver?: string): Promise<NotificationModel[]> {
 	 const result = await getRequest(`../../api/getnotifications.php?username=${nReceiver}`);
 
 	 return JSON.parse(result);
--- client/src/header/notifications/notifications.tsx
+++ client/src/header/notifications/notifications.tsx
@@ -1,18 +1,48 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
 import "./notifications.css";
 import { NotificationModel } from "../../models";
+import { getNotificationList } from "./notification-api";
 
 export interface NotifProps {
-	notifications: NotificationModel[];
+	username?: string;
 }
 
-export class NotificationContainer extends React.Component<NotifProps> {
+export interface NotifState {
+	notificationModel?: NotificationModel[];
+}
+
+export class NotificationContainer extends React.Component<NotifProps, NotifState> {
 	constructor(props: NotifProps) {
 		super(props);
+		this.state = {};
+	}
+
+	componentDidMount() {
+		if (this.props.username) {
+				getNotificationList(this.props.username).then(this.onNotifModelLoaded);
+		}
+	}
+
+	onNotifModelLoaded = (nml: NotificationModel[]) => {
+		this.setState({ notificationModel: nml });
+	};
+
+	componentWillReceiveProps(newProps: NotifProps) {
+		if (this.props.username !== newProps.username) {
+			if (newProps.username) {
+				getNotificationList(newProps.username).then(this.onNotifModelLoaded);
+			} else {
+				this.setState({notificationModel: undefined});
+			}
+		}
 	}
 
 	render() {
-		const notifications = this.props.notifications
+		const { notificationModel } = this.state;
+		if (!this.state.notificationModel)
+			return;
+		const notifications = this.state.notificationModel
 			.map( e => (
 				<div className="dropdown-entry">
 				<h3>{e.firstName} {e.lastName}</h3>
