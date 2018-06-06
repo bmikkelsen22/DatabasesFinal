cc245445fad32faa010a5fc0c0a6eb0f0ef79ff3
haggertt <haggertt@oregonstate.edu>
2018-06-05 18:10:55 -0700

    Adding pages to test sessions

---
+++ sessions/connectvars.php
@@ -0,0 +1,9 @@
+<?php
+  session_start();
+  // Define database connection constants
+  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
+  define('DB_USER', 'cs340_okonekp');
+  define('DB_PASSWORD', '37&?ss9FyF');
+  define('DB_NAME', 'cs340_okonekp');
+  define('CON_STRING', 'mysql:host=classmysql.engr.oregonstate.edu;dname=cs340_okonekp');
+?>
---
+++ sessions/pages.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+//$content holds a 2D array with keys being menu names and values being an array with a subtitle, and content
+	$content = array(
+		"Home Page" => "home.php",
+		"View Groups" => "viewGroups.php",
+		"Account Info" => "accountInfo.php");
+?>
