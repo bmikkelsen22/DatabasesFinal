0f4bfbbeadb1bb72fb32a50db988a102ed2b704a
Parker Okonek <okonekp@oregonstate.edu>
2018-06-07 17:56:34 -0700

    Fixed Header on PHP pages, need admin to accept requests

--- api/getgrouplist.php
+++ api/getgrouplist.php
@@ -9,12 +9,16 @@
         die('Could not connect' . mysqli_error());
     }
 
-	 $username = "";
 
-	if (!isset($_SESSION["username"])) {
-		$username = $_GET['username'];
-	} else {
+	if (isset($_SESSION["username"])) {
 		$username = $_SESSION["username"];
+	} else {
+		$username = $_GET['username'];
+	}
+
+	if (!$username || $username == "") {
+		http_response_code(500);
+		die('Invalid username.');
 	}
 
 	 $query = "SELECT G.gID, G.gDesc, G.gName FROM Groups G, Membership M WHERE M.gID = G.gID AND M.username='$username'";
